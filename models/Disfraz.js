ddd7eca220144094cdc89fb7801eebbfa2414104
Jhon <yojhon@hotmail.es>
2025-02-25 21:59:08 -0500

    tarea asignada de metodos

--- models/Disfraz.js
+++ models/Disfraz.js
@@ -17,6 +17,6 @@ const DisfrazSchema = mongoose.Schema({
         required:true
     }
     //como se guardaran las imagenes para que se muestren en el backed, otro modelo aca
-    //se puede crear otro archivo o otro modelo para la categoria del disfraz. 
+    //se puede crear otro archivo o otro modelo para la categoria del disfraz. Profesor recomendo muller.
 })
 module.exports = mongoose.model('Disfraz', DisfrazSchema)
